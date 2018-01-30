28f195648127438cf35c73e95ae9ae15e5d5d7ca
brliron <brliron@hotmail.fr>
2018-01-30 11:50:47 +0100

    th145arc: fix a typo

--- th145arc/build.sh
+++ th145arc/build.sh
@@ -1,4 +1,4 @@
 g++ th145arc.cpp TFPKArchive.cpp tasofroCrypt.cpp \
 	-Wall -Wextra -Wno-multichar -Wno-parentheses -Wno-sign-compare -std=gnu++11 \
 	-DUNICODE -D_UNICODE \
-	-IMIRACL MIRACL/miracl.a -o th145arc_
+	-IMIRACL MIRACL/miracl.a -o th145arc
