c4172914eb57a0cd57d367c6821993f0ae67740a
brliron <brliron@hotmail.fr>
2018-02-05 12:00:14 +0100

    Add read_pat to the build-all script.
    I forgot it for the 1st release

--- build-all.sh
+++ build-all.sh
@@ -13,9 +13,10 @@ cp Act-Nut-lib/libactnut.dll Act-Nut-lib/print-act-nut.exe bin
 cp bmpfont/bmpfont_extract.exe bmpfont/bmpfont_convert.exe bmpfont/bmpfont_create.exe bmpfont/bmpfont_create_gdi.dll bmpfont/bmpfont_create_gdiplus.dll bin
 cp nhtextool/nhtextool.exe bin
 cp orig_135tk/* bin
+cp read_pat/read_pat.exe bin
 cp TFBMTool-alt/TFBMTool-alt.exe bin
 cp th145arc/th145arc.exe th145arc/fileslist.txt bin
 
 # Copy dependencies
 DEP_DIR=/mingw32/bin
-cp $DEP_DIR/libgcc_s_dw2-1.dll $DEP_DIR/libpng16-16.dll $DEP_DIR/libstdc++-6.dll $DEP_DIR/libwinpthread-1.dll $DEP_DIR/zlib1.dll bin
+cp $DEP_DIR/libgcc_s_dw2-1.dll $DEP_DIR/libjansson-4.dll $DEP_DIR/libpng16-16.dll $DEP_DIR/libstdc++-6.dll $DEP_DIR/libwinpthread-1.dll $DEP_DIR/zlib1.dll bin
