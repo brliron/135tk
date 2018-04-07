c4413415a4e1a2038799aba714fae3052cf218da
brliron <brliron@hotmail.fr>
2018-04-07 15:07:48 +0200

    Build: rename the output directory to 135tk.
    th135tk.zip has more meaning than bin.zip

--- build-all.sh
+++ build-all.sh
@@ -1,6 +1,6 @@
 # Clean
 make -C Act-Nut-Lib clean
-rm -rf bin
+rm -rf 135tk
 
 # Build
 make -C Act-Nut-Lib
@@ -12,17 +12,17 @@ for dir in bmpfont nhtextool read_pat TFBMTool-alt th145arc; do
 done
 
 # Copy binaries
-mkdir -p bin
-cp Act-Nut-lib/libactnut.dll Act-Nut-lib/print-act-nut.exe bin
-cp bmpfont/bmpfont_extract.exe bmpfont/bmpfont_convert.exe bmpfont/bmpfont_create.exe bmpfont/bmpfont_create_gdi.dll bmpfont/bmpfont_create_gdiplus.dll bin
-cp nhtextool/nhtextool.exe bin
-cp orig_135tk/* bin
-cp read_pat/read_pat.exe bin
-cp TFBMTool-alt/TFBMTool-alt.exe TFBMTool-alt/extractBM-alt.exe bin
-cp th145arc/th145arc.exe th145arc/fileslist.txt bin
+mkdir -p 135tk
+cp Act-Nut-lib/libactnut.dll Act-Nut-lib/print-act-nut.exe 135tk
+cp bmpfont/bmpfont_extract.exe bmpfont/bmpfont_convert.exe bmpfont/bmpfont_create.exe bmpfont/bmpfont_create_gdi.dll bmpfont/bmpfont_create_gdiplus.dll 135tk
+cp nhtextool/nhtextool.exe 135tk
+cp orig_135tk/* 135tk
+cp read_pat/read_pat.exe 135tk
+cp TFBMTool-alt/TFBMTool-alt.exe TFBMTool-alt/extractBM-alt.exe 135tk
+cp th145arc/th145arc.exe th145arc/fileslist.txt 135tk
 
 # Copy dependencies
 DEP_DIR=/mingw32/bin
-cp $DEP_DIR/libgcc_s_dw2-1.dll $DEP_DIR/libjansson-4.dll $DEP_DIR/libpng16-16.dll $DEP_DIR/libstdc++-6.dll $DEP_DIR/libwinpthread-1.dll $DEP_DIR/zlib1.dll bin
+cp $DEP_DIR/libgcc_s_dw2-1.dll $DEP_DIR/libjansson-4.dll $DEP_DIR/libpng16-16.dll $DEP_DIR/libstdc++-6.dll $DEP_DIR/libwinpthread-1.dll $DEP_DIR/zlib1.dll 135tk
 
-cp readme.md bin
+cp readme.md 135tk
